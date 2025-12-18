063eefe5a5be2c881f078c647646cf0ed3947ec5
Nikolaj Kurkul <67540024+nick4real@users.noreply.github.com>
2025-12-18 15:05:30 +0100

    feat: new User and Domain entities

---
+++ Services/User/WebStoreUser.Domain/Entities/Session.cs
@@ -0,0 +1,15 @@
+﻿namespace WebStoreUser.Domain.Entities;
+
+public class Session
+{
+    public uint Id { get; init; }
+    public required Guid UserId { get; init; }
+    public required string RefreshTokenHash { get; set; }
+    public required string Salt { get; set; }
+    public DateTime LastUpdated { get; set; }
+    public DateTime Expires { get; set; }
+    public bool IsRevoked { get; set; } = false;
+    public string? Device { get; set; }
+    public string? Ip { get; set; }
+    public string? UserAgent { get; set; }
+}
---
+++ Services/User/WebStoreUser.Domain/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿namespace WebStoreUser.Domain.Entities;
+
+public class User
+{
+    public Guid Id { get; init; }
+    public required string Username { get; init; }
+    public required string Email { get; set; }
+    public required string PasswordHash { get; set; }
+    public DateTime CreatedAt { get; init; }
+}
