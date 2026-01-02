f0fbfa73117df1d2024472c472b5c7801febd4ea
Nikolaj Kurkul <67540024+nick4real@users.noreply.github.com>
2026-01-02 15:35:31 +0100

    refactor: general password regex

--- Services/User/WebStoreUser.Application/Validators/Auth/UserRegisterDtoValidator.cs
+++ Services/User/WebStoreUser.Application/Validators/Auth/UserRegisterDtoValidator.cs
@@ -31,9 +31,8 @@ public class UserRegisterDtoValidator : AbstractValidator<UserRegisterDto>
             .NotEmpty().WithMessage("Password cannot be empty")
             .MinimumLength(8).WithMessage("Password length must be at least 8.")
             .MaximumLength(32).WithMessage("Password length must not exceed 32.")
-            .Matches(@"[A-Z]+").WithMessage("Password must contain at least one uppercase letter.")
-            .Matches(@"[a-z]+").WithMessage("Password must contain at least one lowercase letter.")
-            .Matches(@"[0-9]+").WithMessage("Password must contain at least one number.")
-            .Matches(@"[\!\@\#\$\%\^\?\&\*]+").WithMessage("Password must contain at least one special character.");
+            .Matches(@"^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^?&*]).+$")
+            .WithMessage("Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character.");
+        ;
     }
 }
